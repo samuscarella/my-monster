d43bc31f1cae3daf9a51cf5c06b63afe094eaaa4
samuscarella <stephen.muscarella@kibocommerce.com>
2016-09-12 18:53:00 -0500

    Finished My Little Monster App

---
+++ monster-pet/DragImg.swift
@@ -0,0 +1,54 @@
+//
+//  DragImg.swift
+//  monster-pet
+//
+//  Created by Stephen Muscarella on 8/29/16.
+//  Copyright © 2016 samuscarella. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DragImg: UIImageView {
+    
+    var originalPosition: CGPoint!
+    var dropTarget: UIView?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        originalPosition = self.center
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        if let touch = touches.first {
+            let position = touch.locationInView(self.superview)
+            self.center = CGPointMake(position.x, position.y)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        if let touch = touches.first, let target = dropTarget {
+            
+            let position = touch.locationInView(self.superview)
+            
+            if CGRectContainsPoint(target.frame, position) {
+                
+                // notification to allow logic associated with item drop...
+                NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "onTargetDropped", object: nil))
+            }
+        }
+        
+        self.center = originalPosition
+    }
+}
